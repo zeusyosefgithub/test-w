daf69de863e3c0d83dafbbbd888d185ba1e23464
zeusyosefgithub <midligyosef190@gmail.com>
2024-03-07 14:37:16 +0200

    123d

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const app = express();
-app.get("/", (req, res) => { res.send("Express on Vercel"); });
-const PORT = process.env.PORT || 5000; app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
+
+app.get("/", (req, res) => {
+    res.send("Express on Vercel");
+});
+
+app.post('/', function(req, res) {
+    console.log('receiving data ...');
+    console.log('body is ',req.body);
+    res.send(req.body);
+});
+
+const PORT = process.env.PORT || 5000; app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`); 
+});
