dd1d9eedb88e0ac91940d3ecca92315aeb486498
zeusyosefgithub <midligyosef190@gmail.com>
2024-03-07 16:46:40 +0200

    12ff

--- index.js
+++ index.js
@@ -72,3 +72,5 @@ app.post('/api/sendWhatsupMessage', async (req, res) => {
 const PORT = process.env.PORT || 5000; app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
