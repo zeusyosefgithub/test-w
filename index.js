c011c94cd59dda924afcd823747f0d9e0c645d46
zeusyosefgithub <midligyosef190@gmail.com>
2024-03-07 15:07:26 +0200

    234f

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 app.get("/", (req, res) => {
     res.send("Express on Vercel");
 });
 
-app.post('/req', (req,res) => {
+app.post('/req',urlencodedParser, (req,res) => {
     console.log(req.body);
     res.send('123');
 });
